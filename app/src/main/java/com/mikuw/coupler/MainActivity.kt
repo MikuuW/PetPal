69220352d668c2633c0d058481c406f3158e0444
Mikulas Willaschek <mikulaswillaschek@hotmail.com>
2023-04-19 14:27:58 +0200

    init

--- app/src/main/java/com/mikuw/coupler/MainActivity.kt
+++ app/src/main/java/com/mikuw/coupler/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mains)
+        setContentView(R.layout.activity_main)
 
         // Retrieve the ActionBar object
         val actionBar = supportActionBar
