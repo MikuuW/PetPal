208d35fbe906f01a2fbd8efefde64ef608f82abd
Mikulas Willaschek <mikulaswillaschek@hotmail.com>
2023-04-25 15:05:10 +0200

    add: Changed Styling for using on Real Device

--- app/src/main/java/com/mikuw/coupler/data/Datasource_Firebase_Pets.kt
+++ app/src/main/java/com/mikuw/coupler/data/Datasource_Firebase_Pets.kt
@@ -1,5 +1,6 @@
 package com.mikuw.coupler.data
 
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.mikuw.coupler.model.Pet
 
@@ -7,7 +8,10 @@ class Datasource_Firebase_Pets {
     private val db = FirebaseFirestore.getInstance()
 
     fun loadPets(callback: (List<Pet>) -> Unit) {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
         db.collection("pets")
+            .whereEqualTo("owner", currentUser?.uid)
             .get()
             .addOnSuccessListener { result ->
                 val pets = mutableListOf<Pet>()
@@ -17,12 +21,13 @@ class Datasource_Firebase_Pets {
                     val owner = document.getString("owner") ?: continue
                     val type = document.getString("type") ?: continue
                     val pet = Pet(name, desc, owner, type)
-                    pets.add(Pet(name, desc, owner, type))
+                    pets.add(pet)
                     println(pet)
                 }
                 callback(pets)
             }
     }
+
 }
 
 
